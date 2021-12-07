dc3242f72bf27a1cfb09cff4c452a7f98b5c5f9e
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2021-12-07 20:47:04 -0300

    Aula 19 - PT2 - PI

--- ProgramacaoImperativa/Aula19/ex1.js
+++ ProgramacaoImperativa/Aula19/ex1.js
@@ -1,3 +1,5 @@
+// Parte 1
+
 function Pessoa(nome, sexo, altura) {
     this.nome = nome,
     this.sexo = sexo,
@@ -59,3 +61,60 @@ console.log("Pessoa com menor altura: "+pessoas[0].nome);
 console.log("Pessoa com maior altura: "+pessoas[pessoas.length-1].nome);
 console.log("Média de altura de Mulheres: "+mediaAlturaMulher(pessoas));
 console.log("Quantidade de homens: "+quantidadePorSexo(pessoas, "M"));
+
+// Parte 2
+
+// Função construtora para os espectadores
+function Espectador(idade, opiniao) {
+    this.idade = idade,
+    this.opiniao = opiniao
+};
+
+// Função que faz a média de idade dos espectadores com nota 1 (ótimo)
+function mediaNotaOtimo(array){
+    let result = 0;
+    let a = array.filter((e)=>{
+        return e.opiniao == 3
+    });
+
+    a.forEach(e => {
+        result +=e.idade     
+    });
+
+    return (result/a.length).toFixed(0);
+}
+
+// Retorna a quantidade de pessoas que deram um tipo de nota (1, 2 ou 3)
+function qtdPessoasResposta(array, resposta){
+    let a = array.filter((e)=>{
+        return e.opiniao == resposta
+    });
+
+    return a.length;
+}
+
+// Retorna a porcentagem de pessoas que deram um tipo de nota (1, 2 ou 3)
+function porcentagemNotas(totalPessoas, quantidadeNota){
+    return (quantidadeNota*100/totalPessoas).toFixed(2);
+}
+
+let espectadores = [];
+espectadores.push(new Espectador(18, 2));
+espectadores.push(new Espectador(25, 1));
+espectadores.push(new Espectador(41, 3));
+espectadores.push(new Espectador(32, 3));
+espectadores.push(new Espectador(19, 3));
+espectadores.push(new Espectador(23, 2));
+espectadores.push(new Espectador(29, 2));
+espectadores.push(new Espectador(38, 1));
+espectadores.push(new Espectador(31, 1));
+espectadores.push(new Espectador(27, 3));
+espectadores.push(new Espectador(62, 3));
+espectadores.push(new Espectador(12, 2));
+espectadores.push(new Espectador(19, 1));
+espectadores.push(new Espectador(35, 1));
+espectadores.push(new Espectador(35, 1));
+
+console.log("Média da idade dos espectadores que votaram ótimo: "+mediaNotaOtimo(espectadores));
+console.log("Quantidade de espectadores que responderam regular: "+qtdPessoasResposta(espectadores, 1));
+console.log(porcentagemNotas(espectadores.length, qtdPessoasResposta(espectadores, 2))+"% votaram que o filme é Bom.");
