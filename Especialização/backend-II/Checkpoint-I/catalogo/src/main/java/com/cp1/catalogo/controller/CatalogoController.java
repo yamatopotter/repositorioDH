9bc8939ac90e3df552171fc13ec4e567def787c8
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-05-31 19:02:40 -0300

    wip: Adição de arquivos modificados

--- Especialização/backend-II/Checkpoint-I/catalogo/src/main/java/com/cp1/catalogo/controller/CatalogoController.java
+++ Especialização/backend-II/Checkpoint-I/catalogo/src/main/java/com/cp1/catalogo/controller/CatalogoController.java
@@ -1,2 +1,36 @@
-package com.cp1.catalogo.controller;public class CatalogoController {
+package com.cp1.catalogo.controller;
+
+import com.cp1.catalogo.service.CatalogoService;
+import com.cp1.filmes.entity.Filme;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalogo")
+public class CatalogoController {
+    @Autowired
+    private CatalogoService catalogoService;
+
+    @GetMapping
+    private ResponseEntity<String> work(){
+        return ResponseEntity.ok("It Works!");
+    }
+
+    @GetMapping("/{genero}")
+    public ResponseEntity<List<Filme>> getFilmesByGenero(@PathVariable String genero){
+        List<Filme> filmes = catalogoService.getFilmeByGenero(genero);
+        return ResponseEntity.ok(filmes);
+    }
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<Filme>> getAllFilmes(){
+        List<Filme> filmes = catalogoService.getAllFilmes();
+        return  ResponseEntity.ok(filmes);
+    }
 }
--- Especialização/backend-II/Checkpoint-I/catalogo/src/main/java/com/cp1/catalogo/service/CatalogoService.java
+++ Especialização/backend-II/Checkpoint-I/catalogo/src/main/java/com/cp1/catalogo/service/CatalogoService.java
@@ -1,2 +1,11 @@
-package com.cp1.catalogo.service;public interface CatalogoService {
+package com.cp1.catalogo.service;
+
+import com.cp1.filmes.entity.Filme;
+
+import java.util.List;
+
+public interface CatalogoService {
+    List<Filme> getFilmeByGenero(String genero);
+
+    List<Filme> getAllFilmes();
 }
