0de28cf8b5d5af72e2bc69621121dfe373ee7fd6
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-05-31 04:11:52 -0300

    wip: configurando filme service do checkpoint

--- Especialização/backend-II/Checkpoint-I/filmes/src/main/java/com/cp1/filmes/FilmesApplication.java
+++ Especialização/backend-II/Checkpoint-I/filmes/src/main/java/com/cp1/filmes/FilmesApplication.java
@@ -2,8 +2,12 @@ package com.cp1.filmes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FilmesApplication {
 
 	public static void main(String[] args) {
---
+++ Especialização/backend-II/Checkpoint-I/filmes/src/main/java/com/cp1/filmes/service/FilmeService.java
@@ -0,0 +1,79 @@
+package com.cp1.filmes.service;
+
+import com.cp1.filmes.entity.Filme;
+import com.cp1.filmes.repository.FilmeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class FilmeService {
+    private final FilmeRepository filmeRepository;
+
+    public List<Filme> findAll() {
+        List<Filme> filmes;
+        return filmes = filmeRepository.findAll();
+    }
+
+    public List<Filme> findByGenero(String genero) {
+        List<Filme> filmes;
+        filmes = filmeRepository.findByGenero(genero);
+        if (filmes != null){
+            return filmes;
+        } else {
+            return null;
+        }
+    }
+
+    public Optional<Filme> findById(Long id) {
+        Optional<Filme> filmes;
+
+        filmes = filmeRepository.findById(id);
+        if(filmes.isPresent()){
+            return filmes;
+        } else {
+            return Optional.of(null);
+        }
+    }
+
+    public Optional<Filme> addFilme(Filme filmes) {
+        if (filmes == null) {
+            return Optional.of(null);
+        } else {
+            Filme newFilme = filmeRepository.saveAndFlush(
+                    new Filme(
+                            null,
+                            filmes.getName(),
+                            filmes.getGenero(),
+                            filmes.getUrlStream()
+                    )
+            );
+            return Optional.of(newFilme);
+        }
+    }
+
+    public Optional<Filme> updateFilme (Filme filme) {
+        if (filme == null) {
+            return Optional.of(null);
+        } else {
+            Filme filmeUpdate = new Filme (
+                    filme.getId(),
+                    filme.getName(),
+                    filme.getGenero(),
+                    filme.getUrlStream()
+            );
+            return Optional.of(filmeRepository.saveAndFlush(filmeUpdate));
+        }
+    }
+
+    public Boolean hardDeleteFilme(Long id) {
+        if(filmeRepository.findById(id).isPresent()){
+            filmeRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
