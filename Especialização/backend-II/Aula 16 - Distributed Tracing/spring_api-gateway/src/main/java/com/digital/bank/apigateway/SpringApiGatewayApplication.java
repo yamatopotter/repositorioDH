0bf20b33f54bde6b9ccc480eeb638ffcf7428cf3
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-06-19 20:59:30 -0300

    Adição da aula 16

---
+++ Especialização/backend-II/Aula 16 - Distributed Tracing/spring_api-gateway/src/main/java/com/digital/bank/apigateway/SpringApiGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.digital.bank.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class SpringApiGatewayApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(SpringApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+
+	@Bean
+	public RestTemplateBuilder restTemplateBuilder() {
+		return new RestTemplateBuilder();
+	}
+}
---
+++ Especialização/backend-II/Aula 16 - Distributed Tracing/spring_serviceregistry/src/main/java/com/digital/bank/service/SpringServiceregistryApplication.java
@@ -0,0 +1,22 @@
+package com.digital.bank.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringServiceregistryApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(SpringServiceregistryApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
+}
