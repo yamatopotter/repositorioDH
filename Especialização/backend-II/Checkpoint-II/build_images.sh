4df81cf586b8548461e386893de4df5493537c53
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-07-01 02:51:40 -0300

    Finalizaćão do projeto de backend II

---
+++ Especialização/backend-II/Checkpoint-II/build_images.sh
@@ -0,0 +1,30 @@
+echo "docker-compose down"
+docker-compose down
+echo "docker-compose rm -f"
+docker-compose rm -f
+
+echo "build"
+cd eureka
+mvn clean package -DskipTests
+cd ..
+cd config
+mvn clean package -DskipTests
+cd ..
+cd gateway
+mvn clean package -DskipTests
+cd ..
+cd series
+mvn clean package -DskipTests
+cd ..
+cd filmes
+mvn clean package -DskipTests
+cd ..
+cd catalogo
+mvn clean package -DskipTests
+cd ..
+
+echo "docker-compose build"
+docker-compose build
+
+echo "docker-compose up"
+docker-compose up
